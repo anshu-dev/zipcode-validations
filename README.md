c3040542f3ef6d2081ec9e7a643dba350ef6481d
anshu-dev <anshu902kumar@gmail.com>
2022-06-16 13:02:54 +0530

    completed the task with setup

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 In the project directory, you can run:
 
+### `node version should be ^16.1.0`
+
 ### `npm start`
 
 Runs the app in the development mode.\
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [showError, setShowError] = useState<boolean>(false);
+  const [showSuccess, setShowSuccess] = useState<boolean>(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const currentValue = e.target.value;
+    const isValidZipRegEx = /(^\d{5}$)|(^\d{5}-\d{4}$)/;
+    if (isValidZipRegEx.test(currentValue)) {
+      setShowSuccess(true);
+      setShowError(false);
+    } else {
+      setShowError(true);
+      setShowSuccess(false);
+    }
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App">
+      <form>
+        <input
+          onChange={(e: any) => handleChange(e)}
+          placeholder="Enter Your ZipCode Here...."
+          type="number"
+          style={{ width: "400px", height: "45px", fontSize: "30px" }}
+          name="zipCode"
+          autoComplete="on"
+          autoFocus
+        />
+      </form>
+      <div>
+        {showError && (
+          <p style={{ color: "red" }}>Please Enter a valid zip code</p>
+        )}
+        {showSuccess && (
+          <p style={{ color: "green" }}>You have entered a valid zip code</p>
+        )}
+      </div>
     </div>
   );
 }
